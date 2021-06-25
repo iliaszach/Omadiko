5e75ee7a9a7b38ad21a936ece4398b97221d8c60
George Tzavellas <george.tzavellas@Peoplecert.onmicrosoft.com>
2021-06-25 20:43:04 +0300

    Giorgos Tz: evala ta stoixeia

--- Omadiko.Database/Migrations/Configuration.cs
+++ Omadiko.Database/Migrations/Configuration.cs
@@ -3,7 +3,9 @@ namespace Omadiko.Database.Migrations
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Omadiko.Entities;
+    using Omadiko.Entities.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -56,8 +58,30 @@ namespace Omadiko.Database.Migrations
             }
 
 
+            //========================================================
 
 
+            Student st1 = new Student { Name = "giorgos" };
+            Student st2 = new Student { Name = "giannis" };
+            Student st3 = new Student { Name = "kostas" };
+            Student st4 = new Student { Name = "maria" };
+            Student st5 = new Student { Name = "eleni" };
+
+            Course c1 = new Course { Title = "mathimatika" };
+
+            c1.Students = new List<Student>() { st1, st2 };
+            Course c2 = new Course { Title = "gymnastiki" };
+            c2.Students = new List<Student>() { st1, st2,st3 };
+            Course c3 = new Course { Title = "arxaia" };
+            c3.Students = new List<Student>() { st3 };
+            Course c4 = new Course { Title = "pliroforiki" };
+            c4.Students = new List<Student>() { st1, st2, st3, st4, st5 };
+
+
+            context.Students.AddOrUpdate(x => x.Name, st1, st2, st3, st4, st5);
+            context.Courses.AddOrUpdate(x => x.Title,c1,c2,c3,c4);
+
+            context.SaveChanges();
 
 
 
