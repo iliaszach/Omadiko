50a3d2a13b701d3c075f0cbf334bb75426631b67
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-08 12:56:58 +0300

    Seeding with Providers, Marble objetcs

--- Omadiko.Database/Migrations/Configuration.cs
+++ Omadiko.Database/Migrations/Configuration.cs
@@ -19,18 +19,40 @@ namespace Omadiko.Database.Migrations
 
         protected override void Seed(Omadiko.Database.ApplicationDbContext context)
         {
-            //Product p1 = new Product() { Name = "TurboX", Price = 550 };
-            //Product p2 = new Product() { Name = "HP", Price = 200 };
-            //Product p3 = new Product() { Name = "Samsung", Price = 300 };
-
-            //context.Products.AddOrUpdate(x => new { x.Name }, p1, p2, p3);
+            //Seeding Products
+           
+            Provider p1 = new Provider() { CompanyTitle = "2E Marble", CompanyPhoto="Photo", Phone="6949326800", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            Provider p2 = new Provider() { CompanyTitle = "2E Marìé", CompanyPhoto="Photo", Phone="6949314800", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            Provider p3 = new Provider() { CompanyTitle = "3D Stone Tile & Pavers", CompanyPhoto="Photo", Phone="6941626800", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            Provider p4 = new Provider() { CompanyTitle = "Turkish Marble ", CompanyPhoto="Photo", Phone="6949326840", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            Provider p5 = new Provider() { CompanyTitle = "A Burslem and Son Ltd", CompanyPhoto="Photo", Phone="6949327800", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            Provider p6 = new Provider() { CompanyTitle = "A2Z MARBLE AND TRAVERTINE", CompanyPhoto="Photo", Phone="6947126800", Website="www.cSharp.com", /*Location="Apo GoogleMaps",*/ };
+            context.Providers.AddOrUpdate(x => x.CompanyTitle, p1,p2,p3,p4,p5,p6);
             
+            
+            Marble m1 = new Marble() { Name = "ADRANOS ", Color="White"};
+            m1.Providers = new List<Provider>() { p1, p2 };
+            Marble m2 = new Marble() { Name = "AFYON  ", Color="White"};
+            m2.Providers = new List<Provider>() { p3, p4 };
+            Marble m3 = new Marble() { Name = "AGIA MARINA ", Color= "SEMI-WHITE" };
+            m3.Providers = new List<Provider>() { p5, p6 };
+            Marble m4 = new Marble() { Name = "ALMERA  ", Color= "Pink" };
+            m4.Providers = new List<Provider>() { p1, p2 };
+            Marble m5 = new Marble() { Name = "ARABESCATO", Color= "ALTISSIMO" };
+            m5.Providers = new List<Provider>() { p3, p4 };
+            Marble m6 = new Marble() { Name = "ARABESCATO ", Color = "CERVAIOLE" };
+            m6.Providers = new List<Provider>() { p5, p6 };
 
-
-            Provider p1 = new Provider() { CompanyTitle = "Mitsos" };
-            context.Providers.AddOrUpdate(x => x.CompanyTitle, p1);
+            context.Marbles.AddOrUpdate(x => x.Name, m1, m2, m3, m4, m5, m6);
+            
+            
+            
             context.SaveChanges();
 
+
+
+
+            //Roles
             if (!context.Roles.Any(x => x.Name == "Admin"))
             {
                 var store = new RoleStore<IdentityRole>(context);
