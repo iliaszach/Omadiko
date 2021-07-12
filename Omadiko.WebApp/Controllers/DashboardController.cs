bcd6d9f7f99a438becc8f069fbdb4b9e67615d42
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-12 13:01:39 +0300

    Create DashboardController και ViewDashboard and populate them.

---
+++ Omadiko.WebApp/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using Omadiko.RepositoryServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Omadiko.WebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private ProviderRepository providerRepository = new ProviderRepository();
+        
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var providers = providerRepository.GetAll();
+            return View(providers);
+        }
+    }
+}
