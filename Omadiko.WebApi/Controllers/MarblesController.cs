8a411b3a1fb1b37238cffc441790b5e21f768d7c
thodoris polizos <polizos.thodoris@gmail.com>
2021-08-22 19:35:00 +0300

    marble ΕΤΟΙΜΟ!! Δεν τραβάει κάποια στοιχεία!

--- Omadiko.WebApi/Controllers/MarblesController.cs
+++ Omadiko.WebApi/Controllers/MarblesController.cs
@@ -1,36 +1,47 @@
-﻿using Omadiko.Database;
-using Omadiko.Entities;
-using Omadiko.Entities.Models;
-using Omadiko.RepositoryServices.DataAccess;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using Omadiko.Database;
+using Omadiko.Entities.Models;
 
 namespace Omadiko.WebApi.Controllers
 {
     public class MarblesController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        
-        
 
-        //private UnitOfWork unitOfWork = new UnitOfWork(new ApplicationDbContext());
-        //GET api/marbles
-        
-        public IQueryable<Marble> Get()
+        // GET: api/Marbles
+        public IHttpActionResult GetMarbles()
         {
-          return db.Marbles;
+           
+            var marble = db.Marbles.ToList();
+            return Ok(
+                marble.Select(x => new
+                {
+                    MarbleId = x.MarbleId,
+                    Name = x.Name,
+                    Color = x.Color,
+                    Country = x.Country,
+                    Photo = x.Photo,
+                    Providers = x.Providers.Select(p =>new {p.CompanyTitle })
+                })
+                );
         }
 
-        // GET: api/marble/5
+        // GET: api/Marbles/5
         [ResponseType(typeof(Marble))]
-        public IHttpActionResult GetProduct(int id)
+        public async Task<IHttpActionResult> GetMarble(int id)
         {
-            Marble marble = db.Marbles.Find(id);
+            db.Configuration.ProxyCreationEnabled = false;
+            Marble marble = await db.Marbles.FindAsync(id);
             if (marble == null)
             {
                 return NotFound();
@@ -39,10 +50,88 @@ namespace Omadiko.WebApi.Controllers
             return Ok(marble);
         }
 
-        //GET api/marble/1
-        //public Marble Get(int id)
-        //{
-        //    return db.Marbles.FirstOrDefault(m => m.MarbleId == id);
-        //}
+        // PUT: api/Marbles/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutMarble(int id, Marble marble)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != marble.MarbleId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(marble).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MarbleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Marbles
+        [ResponseType(typeof(Marble))]
+        public async Task<IHttpActionResult> PostMarble(Marble marble)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Marbles.Add(marble);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = marble.MarbleId }, marble);
+        }
+
+        // DELETE: api/Marbles/5
+        [ResponseType(typeof(Marble))]
+        public async Task<IHttpActionResult> DeleteMarble(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            Marble marble = await db.Marbles.FindAsync(id);
+            if (marble == null)
+            {
+                return NotFound();
+            }
+
+            db.Marbles.Remove(marble);
+            await db.SaveChangesAsync();
+
+            return Ok(marble);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MarbleExists(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            return db.Marbles.Count(e => e.MarbleId == id) > 0;
+        }
     }
 }
---
+++ Omadiko.WebApi/Scripts/MyScripts/GetMarbleTable.js
@@ -0,0 +1,55 @@
+﻿$(document).ready(function () {
+    var allMarble = $('#allMarble');
+
+    $('#btn').click(function () {
+        $.ajax({
+            type: "GET",
+            url: "api/Marbles/",
+            dataType: "json",
+            success: function (response) {
+                allMarble.empty();
+                var data = response;
+                console.log(data);
+                data.forEach(appendToTable);
+
+                function appendToTable(data) {
+                    var template = `
+                                    <table id="example-editable-datatables" class="table table-bordered table-hover">
+                                        <thead>
+                                               <tr>
+                                                   <th class="cell-small"></th>
+                                                   <th>Name</th>
+                                                   <th>Color</th>
+                                                   <th>MarbleDescription</th>
+                                                   <th>Country</th>
+                                                   <th>Photo</th>
+                                                   <th>Providers</th>
+                                              </tr>
+                                        </thead>
+                                        <tbody id="soma">
+                                        <tr >
+                                            <td class="text-center">
+                                                <a href="javascript:void(0)" id="delRow1" class="btn btn-xs btn-danger delRow"><i class="fa fa-times"></i></a>
+                                            </td>
+                                            <td>${data.Name}</td>
+                                            <td>${data.Color}</td>
+                                            <td>${data.MarbleDescription}</td>
+                                            <td>${data.Country.Name}</td>
+                                            <td>${data.Photo.Url}</td>
+                                            <td>
+                                                <ul>
+                                                        ${data.Providers.map(x => `<li>${x.CompanyTitle}</li>`).join("")}
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                  </table>`
+                    allMarble.append(template);
+                }
+            }
+        });
+    });
+    $('#btnClear').click(function () {
+        allMarble.empty();
+    });
+});
