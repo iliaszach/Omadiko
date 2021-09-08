b0ac7c4df6e68535007da08d0df6bc66965b7c1c
George Tzavellas <george.tzavellas@Peoplecert.onmicrosoft.com>
2021-09-08 16:25:05 +0300

    home marbles-like , etoimo

--- Omadiko.WebApp/Models/MarbleProviderViewModel/MarbleProviderVM.cs
+++ Omadiko.WebApp/Models/MarbleProviderViewModel/MarbleProviderVM.cs
@@ -8,8 +8,14 @@ namespace Omadiko.WebApp.Models.MarbleProviderViewModel
 {
     public class MarbleProviderVM
     {
+        public MarbleProviderVM()
+        {
+            HelperMarble = new HashSet<Marble>();
+        }
         public List<Marble> Marbles { get; set; }
+        public HashSet<Marble> HelperMarble { get; set; }
         public List<Provider> Providers { get; set; }
+        
 
         //public MarbleProviderVM(IEnumerable<Marble> marble, IEnumerable<Provider> providers)
         //{
